c090f4118f677c6c73f276a21e50a32aad9561e3
Hogan <purmonth@gmail.com>
2024-09-09 16:49:45 -0400

    add files

---
+++ PythonIntro/Lab01/hello2.py
@@ -0,0 +1,3 @@
+# Add userName input and output
+userName = input("Please enter your name: ")
+print(f"Hello, {userName}!")
---
+++ PythonIntro/Lab01/hello3.py
@@ -0,0 +1,4 @@
+# Add userAge input and output
+userName = input("Please enter your name: ")
+userAge = input("Please enter your age: ")
+print(f"Hello, {userName} you are {userAge} years old!")
